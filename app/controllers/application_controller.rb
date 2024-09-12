dab711677631d8d4f11c81316942767a6c80e024
luischino05 <alvaradoarias05@gmail.com>
2024-09-12 11:36:10 -0600

    Fix Devise view error due to turbo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   include CanCan::ControllerAdditions
 
+  def after_sign_in_path_for(resource)
+    root_path # or any other path you prefer
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, alert: "You are not authorized to access this page."
   end
